67e163d69330f6f35ff86242ab25a1a374c98a34
Eula Daniela Yamil <euladanielayamil@gmail.com>
2025-06-04 17:53:55 +0000

    Generador de excusa editado con bootstrap y agregado el boton

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function GeneradorExcusa() {
+function GeneradorExcusa() {
   //write your code here
   let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
   let action = ['ate', 'peed', 'crushed', 'broke'];
@@ -15,11 +15,10 @@ window.onload = function GeneradorExcusa() {
     let ramdomWho = who[Math.floor(Math.random() * who.length)];
     let ramdomWhat = what[Math.floor(Math.random() * what.length)];
     let ramdomWhen = when[Math.floor(Math.random() * when.length)];
-    let ramdomAction = action [Math.floor(Math.random() * action.length)];
+    let ramdomAction = action[Math.floor(Math.random() * action.length)];
 
     let excusa = ramdomWho + " " + ramdomAction + " " + ramdomWhat + " " + ramdomWhen ;
 
-    document.getElementById("excuse").innerHTML = excusa;
-
-  console.log("Hello Rigo from the console!");
+document.getElementById("excuse").innerHTML = excusa;
 };
+document.getElementById("boton").addEventListener("click", GeneradorExcusa);
